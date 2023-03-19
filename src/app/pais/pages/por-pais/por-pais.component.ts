0cc760112c8c9c480abc8ca3135af38351d131f0
“Jhonny <aniceto.0760@gmail.com>
2023-03-19 15:28:54 -0400

    Anexo nuevo proyecto Tic angular Pais

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../service/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styleUrls: ['./por-pais.component.css']
+})
+export class PorPaisComponent {
+
+   termino: string = '';
+   hayError: boolean = false;
+
+   constructor( private PaisService: PaisService) {}
+
+   buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+     
+    this.PaisService.buscarPais( this.termino).subscribe( (resp) =>{
+      
+      console.log(resp);
+    }, (err) => {
+      this.hayError = true;
+    });
+
+   }
+
+}
